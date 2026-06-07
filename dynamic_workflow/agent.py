781165bd6b4e69c66d1e8dd1aa8f4b5b9d641e06
Santos Flores <santosflores@gmail.com>
2026-06-07 09:23:42 -0700

    add dynamic_workflow example

--- dynamic_workflow/agent.py
+++ dynamic_workflow/agent.py
@@ -1,8 +1,27 @@
-from google.adk import Agent
+import logging
+
+from google.adk import Agent, Context, Workflow
+from google.adk.workflow import node
+from typing import Any
 
 AGENT_MODEL = "gemini-3.1-flash-lite"
+logger = logging.getLogger(__name__)
 
-root_agent = Agent(
+
+@node(name="hello_node")
+def hello_node(node_input: Any):
+    logger.info(f"node_input: {node_input}")
+    return "Hello world"
+
+
+@node(rerun_on_resume=True)
+async def workflow(ctx: Context, node_input: str):
+    result = await ctx.run_node(hello_node, node_input=node_input)
+    logger.info(result)
+    return result
+
+
+root_agent = Workflow(
     name="root_agent",
-    model=AGENT_MODEL,
+    edges=[("START", workflow)],
 )
